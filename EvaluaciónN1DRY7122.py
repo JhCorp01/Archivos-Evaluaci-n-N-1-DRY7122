a1da47b46eeabd91ad5f13d74b050e48ffbc1cea
JhCorp01 <128001325+JhCorp01@users.noreply.github.com>
2023-04-13 16:27:56 -0400

    Update EvaluaciónN1DRY7122.py

--- EvaluaciónN1DRY7122.py
+++ EvaluaciónN1DRY7122.py
@@ -48,19 +48,4 @@ for i in range(3):
 
 asignatura = input("Ingrese nombre de la asignatura: ")
 
-integrante = input("Por favor, ingrese el nombre de 1 integrante: ")
-
-acl_num = input("Ingrese el número de ACL IPv4: ")
-
-try:
-    acl_num = int(acl_num)
-except ValueError:
-    print("El número ingresado no es válido.")
-    exit()
-
-if acl_num >= 1 and acl_num <= 99:
-    print("La ACL", acl_num, "es una ACL Estándar.")
-elif acl_num >= 100 and acl_num <= 199:
-    print("La ACL", acl_num, "es una ACL Extendida.")
-else:
-    print("El número ingresado no corresponde a una lista de acceso IPv4.")
+integrante = input("Ingrese el nombre de 1 integrante: ")
